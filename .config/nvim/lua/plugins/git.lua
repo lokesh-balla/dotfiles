2101f2d59d7a59766cb1f80b320eac5f6e5182a1
Lokesh-Balla <Lokesh-Balla@users.noreply.github.com>
2024-06-21 12:44:02 +0530

    update nvim config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,33 +1,13 @@
 return {
     {
-        'lewis6991/gitsigns.nvim',
+        'echasnovski/mini.diff',
+        version = '*',
         opts = {
-            signs = {
-                add = {
-                    text = '+'
-                },
-                change = {
-                    text = '~'
-                },
-                delete = {
-                    text = '_'
-                },
-                topdelete = {
-                    text = '‾'
-                },
-                changedelete = {
-                    text = '~'
-                }
+            view = {
+                style = 'sign',
+                signs = { add = '+', change = '~', delete = '-' },
             },
-            current_line_blame = true,
-            current_line_blame_opts = {
-                virt_text = true,
-                virt_text_pos = 'eol',
-                delay = 1000,
-                ignore_whitespace = false,
-                virt_text_priority = 100
-            },
-            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>'
-        }
+        },
     },
+    { 'tpope/vim-fugitive' },
 }
