2183fd78e1df951b9ab2569996c4f3e7a36515cc
lokesh-balla <22737334+lokesh-balla@users.noreply.github.com>
2025-01-18 22:14:36 +0530

    updating colorscheme nvim to make split border visible

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,13 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
 		config = function()
+			require("tokyonight").setup({
+				on_colors = function(c)
+					c.border = c.blue0
+				end,
+			})
+
 			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
